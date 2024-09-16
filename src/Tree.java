3eb639ece4f6fcdc32bad3f3307e75d79e69d1ff
Rowan Houston <rowan@Rowans-MacBook-Air.local>
2024-09-16 12:46:49 -0400

    Added private attributes for tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,8 @@
+import java.util.List;
+
 public class Tree {
     // TODO complete this Tree class to replicate the implementation from the Tree class in adts.py
+
+    int root;
+    List<Tree> subtrees;
 }
